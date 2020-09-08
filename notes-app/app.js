905e50037baf75465c99ea4c0b8fe11a345d5016
omprakashch3 <omprakashchautala3@gmail.com>
2020-09-08 11:40:33 +0530

    argument parsing with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,53 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 
 const getnotes = require("./notes");
 
-const command = process.argv[2];
+// customize yargs version
+yargs.version("1.1.0");
 
-if (command === "add") {
-  //if we run command node app.js "add" then it will execute this part
-  console.log("adding notes..");
-} else if (command == "remove") {
-  //if we run command node app.js "add" then it will execute this part
-  console.log("removing a tag..");
-}
+// create add command ysing args
+yargs.command({
+  command: "add",
+  describe: "add a new note",
+  handler: function () {
+    console.log("adding a new note!");
+  },
+});
+
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  handler: function () {
+    console.log("removing a notes!");
+  },
+});
+//create list command
+yargs.command({
+  command: "list",
+  describe: "listing out note",
+  handler: function () {
+    console.log("listing out the notes!");
+  },
+});
+//create read command
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function () {
+    console.log("reading a notes!");
+  },
+});
+
+// const command = process.argv[2];
+// console.log(process.argv);
+console.log(yargs.argv);
+
+// if (command === "add") {
+//   //if we run command node app.js "add" then it will execute this part
+//   console.log("adding notes..");
+// } else if (command == "remove") {
+//   //if we run command node app.js "add" then it will execute this part
+//   console.log("removing a tag..");
+// }
